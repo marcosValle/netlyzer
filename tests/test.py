e3c50493f976bf500d10ca190df934cab0deb041
Marcos Valle <marcos.valle01@gmail.com>
2020-02-15 18:37:08 -0300

    Add API keys here

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from netlyzer.main import *
+
+class TestCfg(unittest.TestCase):
+
+    def test_load(self):
+        self.assertEqual(getApiKey('vt'), "<YOUR VT API KEY>", "VT API Key mismatch")
+        self.assertEqual(getApiKey('abuseIPDBKey'), "<YOUR VT API KEY>", "Abuse IP DB API Key mismatch")
+
+    def test_checkDomains(self):
+        self.assertEqual(checkDomains(["https://www.google.com"])[0]['response_code'],1,"Could not check VT")
+
+if __name__ == '__main__':
+    unittest.main()
